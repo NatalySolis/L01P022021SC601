302b268fbdf919a6f954c53afc10b56690a6b1e9
NatalySolis <natycrotez1@outlook.es>
2024-04-06 09:28:59 -0600

    Agregar archivos de proyecto.

---
+++ L01P022021SC601/Models/facultades.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01P022021SC601.Models
+{
+    public class facultades
+    {
+        [Key]
+        public int id { get; set; }
+        public String? facultad { get; set;}
+    }
+}
---
+++ L01P022021SC601/Models/materias.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01P022021SC601.Models
+{
+    public class materias
+    {
+        [Key]
+        public int id { get; set; }
+        public String? materia { get; set; }
+        public int unidades_valorativas { get; set; }
+        public String? estado { get; set; }
+    }
+}
